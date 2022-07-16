9d7d402e3d4ad48545b65c0da1aca63f0f599a15
thatblue <ancientblue@gmail.com>
2022-07-17 00:23:37 +0900

    解説ACしたコード

--- 2022-07-16/arc144_b.rb
+++ 2022-07-16/arc144_b.rb
@@ -1,25 +1,31 @@
+# cf. https://atcoder.jp/contests/arc144/submissions/33274850
+
 n, a, b = gets.chomp.split.map(&:to_i)
 array = gets.chomp.split.map(&:to_i).sort
-max_of_min = array.sum / n
 
-min = array[0]
-(0..(n/2)).each do |smaller_index|
-  previous_diff = array[-1 - smaller_index] - array[smaller_index]
-  loop do
-    smaller_index_value = array[smaller_index] + a
-    larger_index_value = array[-1 - smaller_index] - b
-    diff = array[-1 - smaller_index] - array[smaller_index]
-    if previous_diff < diff || diff == 0
-      break
+max_of_min = array.sum / n
+start_min = array[0]
+
+min_avg = (start_min..max_of_min).bsearch do |tmp_avg|
+  add_count = 0
+  diff_count = 0
+  array.each do |value|
+    diff = value - tmp_avg
+    if diff == 0
+      next
     end
 
-    array[smaller_index] = smaller_index_value
-    array[-1 - smaller_index] = larger_index_value
-
-    previous_diff = diff
-
-    min = [min, [smaller_index_value, larger_index_value].min].max
+    if diff > 0
+      # すべてがtmp_avg以上であることを満たすために値を減らすことが出来る回数
+      diff_count += diff / b
+    else
+      # tmp_avg以上にするために必要な可算回数
+      add_count += (diff / a).abs
+    end
   end
+
+  # 減算可能な回数を可算が必要な回数が上回るタイミングを知りたい
+  diff_count < add_count
 end
 
-p min
+p min_avg == nil ? max_of_min : min_avg - 1
