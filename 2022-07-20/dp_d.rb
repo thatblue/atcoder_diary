9a8a160d30b072d7112587abb28169e40429226b
thatblue <ancientblue@gmail.com>
2022-07-20 23:34:21 +0900

    7/20分のコード

---
+++ 2022-07-20/dp_d.rb
@@ -0,0 +1,21 @@
+n, w = gets.chomp.split.map(&:to_i)
+
+cargos = []
+n.times do
+  cargos << gets.chomp.split.map(&:to_i)
+end
+
+dp = Array.new(n) {Array.new(w + 1, 0)}
+
+n.times do |i|
+  current_weight, current_value = cargos[i]
+  (1..w).each do |weight|
+    if weight - current_weight < 0
+      dp[i][weight] = dp[i - 1][weight]
+    else
+      dp[i][weight] = [dp[i - 1][weight], dp[i - 1][weight - current_weight] + current_value].max
+    end
+  end
+end
+
+p dp[n-1][w]
