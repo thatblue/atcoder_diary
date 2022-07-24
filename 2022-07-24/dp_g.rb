307ba7d721bf25bd5ae0c997b8f624557cc11ba3
thatblue <ancientblue@gmail.com>
2022-07-24 23:07:20 +0900

    解説ACしたコード

--- 2022-07-24/dp_g.rb
+++ 2022-07-24/dp_g.rb
@@ -7,40 +7,33 @@ m.times do
   paths[from] << to
 end
 
+def longest_path_from(start_node, dp, paths, found_nodes)
+  if found_nodes[start_node]
+    return dp[start_node]
+  end
+
+  if paths[start_node].length == 0
+    dp[start_node] = 0
+    return 0
+  end
+
+  longest_path = 0
+  found_nodes[start_node] = true
+  paths[start_node].each do |next_node|
+    longest_path = [longest_path, longest_path_from(next_node, dp, paths, found_nodes) + 1].max
+  end
+
+  dp[start_node] = longest_path
+
+  longest_path
+end
+
 longest_path = 0
 found_nodes = {}
 found_nodes.default = false
+dp = Array.new(n + 1, 0)
 (1..n).each do |start_node|
-  if found_nodes[start_node]
-    next
-  end
-
-  prev_nodes = [start_node]
-  (1..n-1).each do |depth|
-    next_nodes = []
-    prev_nodes.each do |prev_node|
-      paths[prev_node].each do |node|
-        found_nodes[node] = true
-      end
-
-      next_nodes += paths[prev_node]
-    end
-
-    if next_nodes.length == 0
-      longest_path = [longest_path, depth - 1].max
-      break
-    end
-
-    if depth == n - 1
-      # n - 1より長いパスはあり得ない
-      p depth
-      exit
-    end
-
-    next_nodes.uniq!
-
-    prev_nodes = next_nodes
-  end
+  longest_path = [longest_path, longest_path_from(start_node, dp, paths, found_nodes)].max
 end
 
 p longest_path
