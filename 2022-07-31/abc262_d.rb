e70f51378db444485a0d508d0820e93717b68234
thatblue <ancientblue@gmail.com>
2022-08-01 00:12:16 +0900

    RubyのACコードの写経

--- 2022-07-31/abc262_d.rb
+++ 2022-07-31/abc262_d.rb
@@ -1,23 +1,23 @@
+# cf. https://atcoder.jp/contests/abc262/submissions/33686056
+
 VERY_LARGE_PRIME = 998244353
 n = gets.chomp.to_i
 array = gets.chomp.split.map(&:to_i)
 
 sum = 0
 (1..n).each do |i|
-  dp = Array.new(n + 1){Array.new(i + 1) {Array.new(i, 0)}}
-  dp[0][0][0] = 1
-
-  (0..n - 1).each do |j|
-    (0..i).each do|k|
-      (0..i-1).each do|l|
-        dp[j+1][k][l] += dp[j][k][l]
-        if k != i
-          dp[j+1][k+1][(l + array[j]) % i] += dp[j][k][l]
-        end
+  dp = Array.new(i){Hash.new(0)}
+  n.times do|j|
+    (i - 2).downto(0) do |k|
+      break if n - j < i - k - 1
+      dp[k].each do |remain, current_count|
+        dp[k + 1][(array[j] + remain) % i] += current_count
       end
     end
+    dp[0][array[j] % i] += 1
   end
-  sum += dp[n][i][0] % VERY_LARGE_PRIME
+
+  sum += dp[-1][0]
   sum %= VERY_LARGE_PRIME
 end
 
