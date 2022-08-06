1a24a116ffe25219e54ec05fd13766fd70ff95ad
thatblue <ancientblue@gmail.com>
2022-08-06 23:22:44 +0900

    解説ACコード

--- 2022-08-06/abc263_d.rb
+++ 2022-08-06/abc263_d.rb
@@ -1,32 +1,21 @@
+# cf. https://atcoder.jp/contests/abc263/submissions/33827690
 n, l, r = gets.chomp.split.map(&:to_i)
-hash = {}
+array = gets.chomp.split.map(&:to_i)
 
-dp = {}
-dp.default = {}
-dp[0][0] = 0
-diff_to_l = {}
-diff_to_r = {}
-gets.chomp.split.map(&:to_i).each_with_index do |value, index|
-  dp[0][0] += value
-  hash[index + 1]  = value
-  diff_to_l[index + 1] = l - value
-  diff_to_r[index + 1] = r - value
+l_array = [0]
+l_sum = 0
+array.each_with_index do |current, index|
+  l_sum = [l_sum + current, (index + 1) * l].min
+  l_array << l_sum
 end
 
-sum_min = dp[0][0]
-(n + 1).times do |i|
-  max_j = n - i
-  if i > 0
-    current_sum = dp[i - 1][0] + diff_to_l[i]
-    dp[i][0] = current_sum
-    sum_min = [sum_min, current_sum].min
-  end
-
-  (1..max_j).each do |j|
-    current_sum = dp[i][j - 1] + diff_to_r[n - j + 1]
-    dp[i][j] = current_sum
-    sum_min = [sum_min, current_sum].min
-  end
+r_array = [0]
+r_sum = 0
+array.reverse.each_with_index do |current, index|
+  r_sum = [r_sum + current, (index + 1) * r].min
+  r_array << r_sum
 end
 
-p sum_min
+p (n + 1).times.map {
+  l_array.shift + r_array.pop
+}.min
