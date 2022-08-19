8770ab86db7e5fb4b3bc3a914a6aca641b59b6b7
thatblue <ancientblue@gmail.com>
2022-08-20 01:22:04 +0900

    ACコード

--- 2022-08-19/abc143_d.rb
+++ 2022-08-19/abc143_d.rb
@@ -1,15 +1,22 @@
 n = gets.chomp.to_i
 
-combinations = gets.chomp.split.map(&:to_i).combination(3).to_a
+edges = gets.chomp.split.map(&:to_i).sort!
 
 triangle_count = 0
-combinations.each do |edges|
-  edges.sort!
-  if edges[0] + edges[1] <= edges[2]
-    next
-  end
 
-  triangle_count += 1
+i = 0
+while i < n - 2
+  j = i + 1
+  while j < n - 1
+    max_longest_edge = edges[i] + edges[j] - 1
+    too_long_index = edges[j+1..n-1].bsearch_index {|longest_edge| longest_edge > max_longest_edge}
+    too_long_index ||= n - j - 1
+
+    triangle_count += too_long_index
+
+    j += 1
+  end
+  i += 1
 end
 
 p triangle_count
