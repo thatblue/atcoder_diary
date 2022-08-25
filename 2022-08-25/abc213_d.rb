f70aadb8f1349abeba3b72d1d04729abe3ecfd6b
thatblue <ancientblue@gmail.com>
2022-08-26 01:05:18 +0900

    8/25分 ACコード

--- 2022-08-25/abc213_d.rb
+++ 2022-08-25/abc213_d.rb
@@ -1,39 +1,28 @@
 n = gets.chomp.to_i
 
-paths = Array.new(n + 1) { Array.new }
+PATHS = Array.new(n + 1) { [] }
 
 (n-1).times do
   from, to = gets.chomp.split.map(&:to_i)
-  paths[from] << to
-  paths[from].sort!
-  paths[to] << from
-  paths[to].sort!
+  PATHS[from] << to
+  PATHS[to] << from
 end
 
-route = [1]
-prev_town = Hash.new
-prev_town[1] = 1
-last_town = 1
-loop do
-  back_prev_town = true
-  paths[last_town].each do |next_town|
-    if prev_town[next_town].nil?
-      route << next_town
-      prev_town[next_town] = last_town
-      last_town = next_town
-      back_prev_town = false
-      break
-    end
-  end
+PATHS.each do |next_towns|
+  next_towns.sort!
+end
 
-  if back_prev_town
-    if last_town == 1
-      break
-    end
-
-    last_town = prev_town[last_town]
-    route << last_town
+def dfs(current, visited, route)
+  visited[current] = true
+  route << current
+  PATHS[current].each do |next_town|
+    next if visited[next_town]
+    dfs(next_town, visited, route)
+    route << current
   end
 end
+
+route = []
+dfs(1, Array.new(n + 1), route)
 
 puts route.join(" ")
