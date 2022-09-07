0067bdd193fd291a68a4ad9c3177f74c705fec3c
thatblue <ancientblue@gmail.com>
2022-09-07 23:13:49 +0900

    9/7分 ACコード

--- 2022-09-07/typical90_l.rb
+++ 2022-09-07/typical90_l.rb
@@ -1,53 +1,60 @@
-require 'set'
+require "set"
+
+class UnionFind
+  def initialize(n)
+    @parent = Array.new(n + 1) { _1}
+    @size = Array.new(n + 1, 1)
+  end
+
+  def root(v)
+    @parent[v] == v ? v : @parent[v] = root(@parent[v])
+  end
+
+  def merge(u, v)
+    u = root(u)
+    v = root(v)
+
+    return false if u == v
+
+    @size[u] += @size[v]
+    @parent[v] = u
+  end
+
+  def size(u)
+    @size[u]
+  end
+end
+
+def pos_to_value(row, col)
+  (row - 1) * W + col
+end
 
 H, W = gets.chomp.split.map(&:to_i)
 
 q = gets.chomp.to_i
 
-def is_movable(row, col, area)
-  if row > 0 && area.include?([row-1, col])
-    return true
-  elsif col > 0 && area.include?([row, col-1])
-    return true
-  elsif row < H && area.include?([row+1, col])
-    return true
-  elsif col < W && area.include?([row, col+1])
-    return true
-  end
+uf_tree = UnionFind.new(H * W)
+red_cells = Set.new
 
-  false
-end
-
-red_areas = []
 q.times do
   query = gets.chomp.split.map(&:to_i)
 
   if query[0] == 1
     row, col = query[1..2]
+    red_cells.add([row, col])
 
-    new_red_areas = []
-    new_red_area = Set.new([[row, col]])
-    red_areas.each do |red_area|
-      if is_movable(row, col, red_area)
-        new_red_area.merge(red_area)
-      else
-        new_red_areas << red_area
-      end
-    end
-
-    new_red_areas << new_red_area
-    red_areas = new_red_areas
+    uf_tree.merge(pos_to_value(row - 1, col), pos_to_value(row, col)) if row > 0 && red_cells.include?([row - 1, col])
+    uf_tree.merge(pos_to_value(row, col - 1), pos_to_value(row, col)) if row > 0 && red_cells.include?([row, col - 1])
+    uf_tree.merge(pos_to_value(row + 1, col), pos_to_value(row, col)) if row > 0 && red_cells.include?([row + 1, col])
+    uf_tree.merge(pos_to_value(row, col + 1), pos_to_value(row, col)) if row > 0 && red_cells.include?([row, col + 1])
   elsif query[0] == 2
     a_row, a_col, b_row, b_col = query[1..4]
 
-    detected_common_area = false
-    red_areas.each do |red_area|
-      if red_area.include?([a_row, a_col]) && red_area.include?([b_row, b_col])
-        detected_common_area = true
-        break
-      end
+    unless red_cells.include?([a_row, a_col]) && red_cells.include?([b_row, b_col])
+      puts "No"
+      next
     end
 
-    puts detected_common_area ? "Yes" : "No"
+    puts uf_tree.root(pos_to_value(a_row, a_col)) == uf_tree.root(pos_to_value(b_row, b_col)) ? "Yes" : "No"
   end
 end
