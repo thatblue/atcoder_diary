2706ada4cbbf1868839440823604e7c165fe4b40
thatblue <ancientblue@gmail.com>
2022-09-10 23:00:31 +0900

    解説ACコード

--- 2022-09-10/abc268_c.rb
+++ 2022-09-10/abc268_c.rb
@@ -1,17 +1,13 @@
 n = gets.chomp.to_i
-dishes = gets.chomp.split.map.with_index do |dish, index|
-  (dish.to_i - index + n) % n
+dishes = gets.chomp.split.map(&:to_i)
+
+satisfied = Array.new(n, 0)
+
+dishes.each_with_index do |dish, i|
+  offset = (dish - i) % n
+  satisfied[(offset - 1) % n] += 1
+  satisfied[(offset) % n] += 1
+  satisfied[(offset + 1) % n] += 1
 end
 
-diffs = dishes.tally.sort.to_h
-diffs.default = 0
-
-max_satisfied = 0
-diffs.each do |offset, count|
-  offset_before = offset == 0 ? n - 1 : offset - 1
-  offset_after = offset == n - 1 ? 0 : offset + 1
-
-  max_satisfied = [max_satisfied, count + diffs[offset_before] + diffs[offset_after]].max
-end
-
-p max_satisfied
+p satisfied.max
