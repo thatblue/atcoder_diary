859ea1332c1c51f5cd5499f70422108ae568d194
thatblue <ancientblue@gmail.com>
2022-09-23 00:30:30 +0900

    ACコード

--- 2022-09-22/typical90_ab.rb
+++ 2022-09-22/typical90_ab.rb
@@ -1,6 +1,7 @@
 n = gets.chomp.to_i
+COORD_MAX = 1000
 
-imos = Array.new(1001) {Array.new(1001, 0)}
+imos = Array.new(COORD_MAX + 1) { Array.new(COORD_MAX + 1, 0) }
 
 n.times do
   l_x, l_y, r_x, r_y = gets.chomp.split.map(&:to_i)
@@ -11,24 +12,20 @@ n.times do
   imos[r_x][l_y] -= 1
 end
 
-0.upto(1000) do |x|
-  current_num = imos[x][0]
-  1.upto(1000) do |y|
-    imos[x][y] += current_num
-    current_num = imos[x][y]
+0.upto(COORD_MAX) do |x|
+  1.upto(COORD_MAX) do |y|
+    imos[x][y] += imos[x][y - 1]
   end
 end
 
-papers = Array.new(n + 1, 0)
-0.upto(1000) do |y|
-  current_num = imos[0][y]
-  1.upto(1000) do |x|
-    imos[x][y] += current_num
-    current_num = imos[x][y]
-    papers[current_num] += 1
+0.upto(COORD_MAX) do |y|
+  1.upto(COORD_MAX) do |x|
+    imos[x][y] += imos[x - 1][y]
   end
 end
+
+ans = imos.flatten.tally
 
 1.upto(n) do |i|
-  p papers[i]
+  puts ans[i] || 0
 end
