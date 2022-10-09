52b51280e785abaaba4d826fe06ad311d5026064
thatblue <ancientblue@gmail.com>
2022-10-09 23:22:29 +0900

    解説の参考コードの写経(TLEする)

--- 2022-10-09/typical90_aq.rb
+++ 2022-10-09/typical90_aq.rb
@@ -5,6 +5,9 @@ RIGHT = 1
 BOTTOM = 2
 LEFT = 3
 
+X_INCREMENTS = [1, 0, -1, 0]
+Y_INCREMENTS = [0, 1, 0, -1]
+
 H, W = gets.chomp.split.map(&:to_i)
 r_s, c_s = gets.chomp.split.map {|v| v.to_i - 1 }
 r_t, c_t = gets.chomp.split.map {|v| v.to_i - 1 }
@@ -29,49 +32,29 @@ def visitable(x, y)
   true
 end
 
-def visit(x, y)
-  unless visitable(x, y)
-    return false
-  end
-
-  if VISITED_MAP[x][y][TOP] == Float::INFINITY
-    from_top_cost = visitable(x + 1, y) ? VISITED_MAP[x + 1][y][TOP] : Float::INFINITY
-    from_right_cost = visitable(x, y - 1) ? VISITED_MAP[x][y - 1][RIGHT] : Float::INFINITY
-    from_bottom_cost = visitable(x - 1, y) ? VISITED_MAP[x - 1][y][BOTTOM] : Float::INFINITY
-    from_left_cost = visitable(x, y + 1) ? VISITED_MAP[x][y + 1][LEFT] : Float::INFINITY
-    min_cost = [from_top_cost, from_right_cost, from_bottom_cost, from_left_cost].min
-
-    VISITED_MAP[x][y][TOP] = [from_top_cost, min_cost + 1].min
-    VISITED_MAP[x][y][RIGHT] = [from_right_cost, min_cost + 1].min
-    VISITED_MAP[x][y][BOTTOM] = [from_bottom_cost, min_cost + 1].min
-    VISITED_MAP[x][y][LEFT] = [from_left_cost, min_cost + 1].min
-    return true
-  end
-
-  false
-end
-
 VISITED_MAP[r_s][c_s][TOP] = 0
 VISITED_MAP[r_s][c_s][RIGHT] = 0
 VISITED_MAP[r_s][c_s][BOTTOM] = 0
 VISITED_MAP[r_s][c_s][LEFT] = 0
-current_nodes = Set.new([[r_s, c_s]])
+search_nodes = [[r_s, c_s, TOP], [r_s, c_s, RIGHT], [r_s, c_s, BOTTOM], [r_s, c_s, LEFT]]
 
-while current_nodes.length > 0
-  next_nodes = Set.new
-  current_nodes.each do |current_node|
-    x, y = current_node
-    next_nodes.add([x - 1, y]) if visit(x - 1, y)
-    next_nodes.add([x, y - 1]) if visit(x, y - 1)
-    next_nodes.add([x, y + 1]) if visit(x, y + 1)
-    next_nodes.add([x + 1, y]) if visit(x + 1, y)
+until search_nodes.empty?
+  current_node = search_nodes.shift
+  x, y, direction = current_node
+
+  4.times do |next_direction|
+    next_x = x + X_INCREMENTS[next_direction]
+    next_y = y + Y_INCREMENTS[next_direction]
+    cost = VISITED_MAP[x][y][direction] + (direction == next_direction ? 0 : 1)
+    if visitable(next_x, next_y) && VISITED_MAP[next_x][next_y][next_direction] > cost
+      VISITED_MAP[next_x][next_y][next_direction] = cost
+      if direction == next_direction
+        search_nodes.push([next_x, next_y, next_direction])
+      else
+        search_nodes.unshift([next_x, next_y, next_direction])
+      end
+    end
   end
-
-  if next_nodes.include?([r_t, c_t])
-    break
-  end
-
-  current_nodes = next_nodes
 end
 
 p VISITED_MAP[r_t][c_t].min
