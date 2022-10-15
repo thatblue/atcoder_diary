1bb775d62a9e41459b55bab8397f7546f4958af5
thatblue <ancientblue@gmail.com>
2022-10-15 23:22:52 +0900

    A問題のupsolve

--- 2022-10-15/abc273_a.rb
+++ 2022-10-15/abc273_a.rb
@@ -1,16 +1,2 @@
 n = gets.chomp.to_i
-
-def recursive_func(x)
-  if x < 2
-    return 1
-  end
-
-  x * recursive_func(x - 1)
-end
-
-if n < 2
-  p 1
-  exit
-end
-
-p n * recursive_func(n - 1)
+p Math.gamma(n + 1).to_i
