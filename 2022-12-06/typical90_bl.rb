d80358daa21c95531a103f14033508080b9c2586
thatblue <ancientblue@gmail.com>
2022-12-07 01:06:13 +0900

    TLEのコード

--- 2022-12-06/typical90_bl.rb
+++ 2022-12-06/typical90_bl.rb
@@ -1,31 +1,9 @@
 n, q = gets.chomp.split.map(&:to_i)
 
 heights = gets.chomp.split.map(&:to_i)
-inconvenience = 0
+diffs = Array.new(n - 1)
 (n-1).times do |i|
-  inconvenience += (heights[i] - heights[i + 1]).abs
-end
-imos = Array.new(n, 0)
-
-def diff(l_init, l_diff, r_init, r_diff, new_diff)
-  current_l = l_init + l_diff
-  current_r = r_init + r_diff
-
-  if current_l < current_r
-    # 高くなる
-    if new_diff > 0
-      return new_diff
-    else
-      return ((current_l - current_r).abs - new_diff).abs
-    end
-  else
-    # 低くなる
-    if new_diff < 0
-      return new_diff
-    else
-      return ((current_l - current_r).abs - new_diff).abs
-    end
-  end
+  diffs[i] = heights[i + 1] - heights[i]
 end
 
 q.times do
@@ -34,16 +12,12 @@ q.times do
   r_index = r - 1
 
   if l_index > 0
-    inconvenience += diff(heights[l_index - 1], imos[l_index - 1], heights[l_index], imos[l_index], v)
+    diffs[l_index - 1] += v
   end
 
   if r_index < n - 1
-    inconvenience += diff(heights[r_index], imos[r_index], heights[r_index + 1], imos[r_index + 1], -v)
+    diffs[r_index] -= v
   end
 
-  puts inconvenience
-
-  l_index.upto(r_index) do |index|
-    imos[index] += v
-  end
+  puts diffs.map { |x| x.abs }.sum
 end
