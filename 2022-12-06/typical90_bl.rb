ded6ee82719a61f29e81a92f680aab33c27a90eb
thatblue <ancientblue@gmail.com>
2022-12-07 01:11:38 +0900

    ACしたコード

--- 2022-12-06/typical90_bl.rb
+++ 2022-12-06/typical90_bl.rb
@@ -2,8 +2,10 @@ n, q = gets.chomp.split.map(&:to_i)
 
 heights = gets.chomp.split.map(&:to_i)
 diffs = Array.new(n - 1)
+diffs_sum = 0
 (n-1).times do |i|
   diffs[i] = heights[i + 1] - heights[i]
+  diffs_sum += diffs[i].abs
 end
 
 q.times do
@@ -12,12 +14,14 @@ q.times do
   r_index = r - 1
 
   if l_index > 0
+    diffs_sum += (diffs[l_index - 1] + v).abs - (diffs[l_index - 1]).abs
     diffs[l_index - 1] += v
   end
 
   if r_index < n - 1
+    diffs_sum += (diffs[r_index] - v).abs - (diffs[r_index]).abs
     diffs[r_index] -= v
   end
 
-  puts diffs.map { |x| x.abs }.sum
+  puts diffs_sum
 end
