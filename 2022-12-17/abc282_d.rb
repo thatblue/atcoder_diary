93465d7897bee823c3dcc4b5195c28a35e8a588f
thatblue <ancientblue@gmail.com>
2022-12-19 00:08:16 +0900

    Hashを使ってリファクタ

--- 2022-12-17/abc282_d.rb
+++ 2022-12-17/abc282_d.rb
@@ -1,5 +1,8 @@
 # 解説の写経
 
+RED = 1
+BLUE = -1
+
 n, m = gets.chomp.split.map(&:to_i)
 
 $paths = Array.new(n + 1) { Array.new }
@@ -12,25 +15,23 @@ end
 $color = Array.new(n + 1)
 
 def dfs(current_node, prev_node, current_color)
-  result = [0, 0]
+  result = { RED => 0, BLUE => 0}
 
   $color[current_node] = current_color
-  result[0] += 1 if current_color == 1
-  result[1] += 1 unless current_color == 1
+  result[current_color] += 1
 
   $paths[current_node].each do |next_node|
     # 既に訪問済みのノードで、別の色で塗ってあれば検証不要なのでスキップ
     next if next_node == prev_node || $color[next_node] == -current_color
 
     # 隣り合うはずのノードなのに同じ色で塗られている場合は2部グラフではない
-    return [-1, -1] if $color[next_node] == current_color
+    return { RED => -1, BLUE => -1 } if $color[next_node] == current_color
 
     next_result = dfs(next_node, current_node, -current_color)
     # 次のノード(以降)で2部グラフではないことが検出された
-    return [-1, -1] if next_result[0] == -1
+    return { RED => -1, BLUE => -1 } if next_result[RED] == -1
 
-    result[0] += next_result[0]
-    result[1] += next_result[1]
+    result.merge!(next_result) {|_, current_value, next_value| current_value + next_value }
   end
 
   result
@@ -39,20 +40,20 @@ end
 # ありうる頂点の全組み合わせから実在するMを減らしておく
 answer = n * (n - 1) / 2 - m
 1.upto(n) do |i|
-  if $color[i].nil?
-    # 未走査であればこの点を起点としてグラフを走査する
-    result = dfs(i, -1, 1)
-    if result[0] == -1
-      # 1つでも2部グラフではない連結成分があれば条件が成立しないので終了
-      puts 0
-      exit
-    end
+  # 走査済みであればスキップ
+  next unless $color[i].nil?
 
-    # 同じ連結成分内の同じ色どうしの組み合わせを減らす
-    # 別の連結成分に繋ぐ分にはどう繋いでも2部グラフになる(らしい)
-    answer -= result[0] * (result[0] - 1) / 2
-    answer -= result[1] * (result[1] - 1) / 2
+  # 未走査であればこの点を起点としてグラフを走査する
+  result = dfs(i, -1, RED)
+  if result[RED] == -1
+    # 1つでも2部グラフではない連結成分があれば条件が成立しないので終了
+    puts 0
+    exit
   end
+
+  # 同じ連結成分内の同じ色どうしの組み合わせ分を減らす
+  # 別の連結成分に繋ぐ分にはどう繋いでも2部グラフになる
+  result.each {|_, value| answer -= value * (value  - 1) / 2 }
 end
 
 # 最後まで残った組み合わせが答えになる
