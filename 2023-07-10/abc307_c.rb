786f7e15d38d3bd82be8dbf75d78de30cabc398d
thatblue <ancientblue@gmail.com>
2023-07-13 02:03:12 +0900

    ACしたコード

--- 2023-07-10/abc307_c.rb
+++ 2023-07-10/abc307_c.rb
@@ -4,7 +4,7 @@ def read_sheet
   h, w = gets.chomp.split.map(&:to_i)
   sheet = []
   h.times do
-    sheet << gets.chomp.chars.map { |v| v == '#' }
+    sheet << gets.chomp.chars.map { |v| v == '#' ? '1' : '0' }.join.to_i(2)
   end
 
   [h, w, sheet]
@@ -14,35 +14,27 @@ HA, WA, sheet_a = read_sheet
 HB, WB, sheet_b = read_sheet
 HX, WX, tmp = read_sheet
 
-sheet_x = Array.new(10) { Array.new(30, false) }
+sheet_x = Array.new(10, 0)
 HX.times do |i|
-  sheet_x << Array.new(10, false) + tmp[i] + Array.new(20 - WX, false)
+  sheet_x << (tmp[i].to_s(2) + '0' * (20 - WX)).to_i(2)
 end
-sheet_x += Array.new(20 - HX) { Array.new(30, false) }
+sheet_x += Array.new(20 - HX, 0)
 
 START_HX = 10
 START_WX = 10
-
-def inside_x?(i, j)
-  i.between?(START_HX, START_HX + HX - 1) && j.between?(START_WX, START_WX + WX - 1)
-end
 
 (START_HX - HA + 1).upto(19) do |ai|
   (START_WX - WA + 1).upto(19) do |aj|
     (START_HX - HB + 1).upto(19) do |bi|
       (START_WX - WB + 1).upto(19) do |bj|
-        combined = Array.new(30) { Array.new(30, false) }
+        combined = Array.new(30, 0)
 
         HA.times do |i|
-          WA.times do |j|
-            combined[ai + i][aj + j] = true if sheet_a[i][j]
-          end
+          combined[ai + i] |= sheet_a[i] << (30 - (aj + WA))
         end
 
         HB.times do |i|
-          WB.times do |j|
-            combined[bi + i][bj + j] = true if sheet_b[i][j]
-          end
+          combined[bi + i] |= sheet_b[i] << (30 - (bj + WB))
         end
 
         if combined == sheet_x
