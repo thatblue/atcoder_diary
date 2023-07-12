4b5b1d33a90c9a03c91464f79af8369e0964a8b4
thatblue <ancientblue@gmail.com>
2023-07-13 01:28:38 +0900

    解説の写経(全部TLEになる)

--- 2023-07-10/abc307_c.rb
+++ 2023-07-10/abc307_c.rb
@@ -1,61 +1,56 @@
-h_a, w_a = gets.chomp.split.map(&:to_i)
-sheet_a = []
-h_a.times do
-  sheet_a << gets.chomp.chars.map { |v| v == '#' }
+# cf. https://atcoder.jp/contests/abc307/submissions/42920356
+
+def read_sheet
+  h, w = gets.chomp.split.map(&:to_i)
+  sheet = []
+  h.times do
+    sheet << gets.chomp.chars.map { |v| v == '#' }
+  end
+
+  [h, w, sheet]
 end
 
-h_b, w_b = gets.chomp.split.map(&:to_i)
-sheet_b = []
-h_b.times do
-  sheet_b << gets.chomp.chars.map { |v| v == '#' }
+HA, WA, sheet_a = read_sheet
+HB, WB, sheet_b = read_sheet
+HX, WX, sheet_x = read_sheet
+
+START_HX = 10
+START_WX = 10
+
+def inside_x?(i, j)
+  i.between?(START_HX, START_HX + HX - 1) && j.between?(START_WX, START_WX + WX - 1)
 end
 
-h_x, w_x = gets.chomp.split.map(&:to_i)
-sheet_x = []
-h_x.times do
-  sheet_x << gets.chomp.chars.map { |v| v == '#' }
-end
+20.times do |ai|
+  20.times do |aj|
+    20.times do |bi|
+      20.times do |bj|
+        combined = Array.new(30) { Array.new(30, false) }
 
-height = ([h_a, h_b].max - 1) * 2 + h_x
-width = ([w_a, w_b].max - 1) * 2 + w_x
-h_start = [h_a, h_b].max - 1
-h_end = h_start + h_x - 1
-w_start = [w_a, w_b].max - 1
-w_end = w_start + w_x - 1
-
-offset_h_a = height - h_a
-offset_w_a = width - w_a
-offset_h_b = height - h_b
-offset_w_b = width - w_b
-
-def black?(sheet, h, w, i, j)
-  return false unless i.between?(0, h - 1) && j.between?(0, w - 1)
-
-  sheet[i][j]
-end
-
-0.upto(offset_h_a) do |i_a|
-  0.upto(offset_w_a) do |j_a|
-    0.upto(offset_h_b) do |i_b|
-      0.upto(offset_w_b) do |j_b|
-        success = true
-
-        height.times do |i|
-          width.times do |j|
-            a_is_black = black?(sheet_a, h_a, w_a, i - i_a, j - j_a)
-            b_is_black = black?(sheet_b, h_b, w_b, i - i_b, j - j_b)
-            if i.between?(h_start, h_end) && j.between?(w_start, w_end)
-              success &= sheet_x[i - h_start][j - w_start] == a_is_black || b_is_black
-            else
-              success &= !a_is_black && !b_is_black
-            end
-
-            break unless success
+        HA.times do |i|
+          WA.times do |j|
+            combined[ai + i][aj + j] = true if sheet_a[i][j]
           end
-          break unless success
         end
 
-        if success
+        HB.times do |i|
+          WB.times do |j|
+            combined[bi + i][bj + j] = true if sheet_b[i][j]
+          end
+        end
+
+        result = true
+        30.times do |i|
+          30.times do |j|
+            if inside_x?(i, j)
+              result &= combined[i][j] == sheet_x[i - START_HX][j - START_WX]
+            else
+              result &= !combined[i][j]
+            end
+          end
+        end
+
+        if result
           puts 'Yes'
           exit
         end
