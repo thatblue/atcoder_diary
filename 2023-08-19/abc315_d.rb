c8da8b74239435c44f3a8fccf5a548637f750f09
thatblue <ancientblue@gmail.com>
2023-08-20 00:37:08 +0900

    D問題のupsolve ダメ(WA)だった愚直っぽい解

--- 2023-08-19/abc315_d.rb
+++ 2023-08-19/abc315_d.rb
@@ -1,14 +1,38 @@
 h, w = gets.chomp.split.map(&:to_i)
 
 cookies = []
-
 h.times do
-  row = gets.chomp
-  cookies << row.chars if row.squeeze >= 2
+  cookies << gets.chomp.chars
 end
 
-transposed = cookies.transpose
+cookies_count = h * w
+current_h = h
+current_w = w
+if h < w
+  cookies = cookies.transpose if h < w
+  current_h = w
+  current_w = h
+end
 
-transposed.each do |col|
-  
+loop do
+  next_cookies = []
+  next_h = 0
+  next_w = 0
+  cookies.each do |row|
+    next_w = row.length
+    squeezed = row.join.squeeze.length
+    next if next_w > 1 && squeezed == 1
+
+    next_cookies << row
+    next_h += 1
+  end
+
+  current_cookies_count = next_h * next_w
+  if cookies_count == current_cookies_count
+    puts current_cookies_count
+    exit
+  end
+
+  cookies_count = current_cookies_count
+  cookies = next_cookies.transpose
 end
