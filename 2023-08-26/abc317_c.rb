55bc84f8e191a7c96152e2a09932b098c868629c
thatblue <ancientblue@gmail.com>
2023-08-26 23:18:12 +0900

    C問題のupsolve

--- 2023-08-26/abc317_c.rb
+++ 2023-08-26/abc317_c.rb
@@ -1,5 +1,6 @@
 n, m = gets.chomp.split.map(&:to_i)
 
+
 $map = Array.new(n + 1) { Hash.new }
 m.times do
   a, b, c = gets.chomp.split.map(&:to_i)
@@ -7,22 +8,25 @@ m.times do
   $map[b][a] = c
 end
 
-def dfs(current)
-  $visited[current] = true
-  max_days = 0
-  $map[current].each do |next_town, days|
-    next if $visited[next_town]
-    max_days = [max_days, dfs(next_town) + days].max
-  end
-  $visited.delete(current)
+def dfs(current, visited_nodes)
+  visited_nodes |= (1 << current)
+  return $memo[current][visited_nodes] if $memo[current][visited_nodes]
 
-  return max_days
+  max_distance = 0
+  $map[current].each do |next_town, distance|
+    next if visited_nodes[next_town] == 1
+
+    max_distance = [max_distance, dfs(next_town, visited_nodes) + distance].max
+  end
+
+  $memo[current][visited_nodes] = max_distance
+  return max_distance
 end
 
-$visited = {}
 ans = 0
+$memo = Array.new(n + 1) { Hash.new }
 (1..n).each do |start_town|
-  ans = [ans, dfs(start_town)].max
+  ans = [ans, dfs(start_town, 0)].max
 end
 
 puts ans
