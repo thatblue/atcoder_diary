fe78d816d9f8221de93f922faa9415303fb75723
thatblue <ancientblue@gmail.com>
2023-09-03 00:25:56 +0900

    Dのupsolve

--- 2023-09-02/abc318_d.rb
+++ 2023-09-02/abc318_d.rb
@@ -1,41 +1,36 @@
 n = gets.chomp.to_i
 
-$graph = Array.new(n+1) { Array.new(n+1, 0) }
+graph = Array.new(n+1) { Array.new(n+1, 0) }
 
 1.upto(n-1) do |i|
-  gets.chomp.split.map(&:to_i).each_with_index do |v, index|
+  gets.chomp.split.each_with_index do |v, index|
     j = index + i + 1
-    $graph[i][j] = v
-    $graph[j][i] = v
+    weight = v.to_i
+    graph[i][j] = weight
+    graph[j][i] = weight
   end
 end
 
 nodes = (1..n).to_a
 
-$memo = {}
-def dfs(current_nodes, bit)
-  return 0 if current_nodes.empty?
-
-  return $memo[bit] unless $memo[bit].nil?
-
-  pick_node_count = current_nodes.count.odd? ? 1 : 2
-  max_weight = 0
-  current_nodes.combination(pick_node_count).to_a.each do |picked_nodes|
-    picked_weight = 0
-    if pick_node_count == 2
-      a, b = picked_nodes
-      picked_weight = $graph[a][b]
-    end
-    next_node
-    picked_nodes.each do |node|
-
-    end
-    current_weight = picked_weight + dfs(current_nodes - picked_nodes)
-
-    max_weight = [current_weight, max_weight].max
+dp = {}
+dp[0] = 0
+fullbit = 2**n-1
+(1..fullbit).each do |bit|
+  active_nodes = []
+  n.times do |i|
+    active_nodes << i + 1 if bit[i] == 1
   end
 
-  $memo[bit] = max_weight
+  next if active_nodes.count.odd?
+
+  dp[bit] = 0
+
+  active_nodes.combination(2).each do |latest_choices|
+    a, b = latest_choices
+    previous_bit = (active_nodes - latest_choices).inject(0) { |result, digit| result | (1 << (digit-1)) } || 0
+    dp[bit] = [dp[bit], dp[previous_bit] + graph[a][b]].max
+  end
 end
 
-puts dfs((1..n).to_a, 2**n-1)
+puts dp.values.max
