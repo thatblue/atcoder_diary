9c4fe13ccdf920b1453a9430e6983586b0a3e614
thatblue <ancientblue@gmail.com>
2023-09-17 00:19:10 +0900

    C問題のupsolve

--- 2023-09-16/abc320_c.rb
+++ 2023-09-16/abc320_c.rb
@@ -1,41 +1,29 @@
 m = gets.chomp.to_i
-
 reels = []
 common_panels = nil
+shortests = {}
+common_panels = nil
 
-reels << gets.chomp.chars.map(&:to_i)
-
-reels << gets.chomp.chars.map(&:to_i)
-common_panels = reels[0].intersection(reels[1])
-
-reels << gets.chomp.chars.map(&:to_i)
-common_panels = common_panels.intersection(reels[2])
+reels << gets.chomp * 3
+reels << gets.chomp * 3
+reels << gets.chomp * 3
+common_panels = reels[0].chars.intersection(reels[1].chars, reels[2].chars)
 
 if common_panels.empty?
   puts -1
   exit
 end
 
-common_reel_positions = Hash.new {|hash, key| hash[key] = {} }
-
-reels.each_with_index do |reel, reel_index|
-  reel.each_with_index do |panel, time|
-    next unless common_panels.include?(panel)
-    common_reel_positions[panel][time] = reel_index
+shortest_times = Hash.new(Float::INFINITY)
+(0..2).to_a.permutation.each do |order|
+  time = 0
+  common_panels.each do |panel|
+    first, second, third = order
+    time = reels[first].index(panel)
+    time = reels[second].index(panel, time + 1)
+    time = reels[third].index(panel, time + 1)
+    shortest_times[panel] = [shortest_times[panel], time].min
   end
 end
 
-
-fastest_bingos = common_panels.map { |v| [v, m] }.to_h
-common_panels.each do |panel|
-  panels = {}
-  common_reel_positions[panel].keys.sort.each do |time|
-    panel = common_reel_positions[time]
-    next if panels.include?(panel)
-    panels[panel] = time
-  end
-
-  
-end
-
-puts fastest_bingos.values.min
+puts shortest_times.values.min
