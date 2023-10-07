160e622f0910ae54f91cc23d1a243d2d92fe3423
thatblue <ancientblue@gmail.com>
2023-10-07 23:57:16 +0900

    ABC323-C upsolve

--- 2023-10-07/abc323_c.rb
+++ 2023-10-07/abc323_c.rb
@@ -5,12 +5,11 @@ point_indexes = Hash.new {|hash, key| hash[key] = []}
 points.each_with_index do |point, index|
   point_indexes[point] << index
 end
-point_reverse_orders = point_indexes.keys.reverse
+point_reverse_order = point_indexes.keys.sort.reverse
 
-current_max = 0
+score_max = 0
 results = []
 scores = []
-score_distribution = Hash.new(0)
 n.times do |index|
   player_id = index + 1
   player_results = gets.chomp.chars.map { |v| v == 'o' }
@@ -20,29 +19,27 @@ n.times do |index|
 
   results << player_results
   scores << player_score
-  score_distribution[player_score] += 1
-
-  current_max = [current_max, player_score].max
+  score_max = [score_max, player_score].max
 end
 
-scores.each_with_index do |score, player_id|
-  if current_max == score && score_distribution[score] == 1
-    # 単独トップの場合はそのままでよい
+scores.each_with_index do |score, player_index|
+  if score_max == score
+    # 自分がトップの場合はそのままでよい(同率1位は制約上ありえない)
     puts 0
     next
   end
 
-  diff = current_max - score
+  diff = score_max - score
   ans = 0
   ans_detected = false
   # 点数の高い方から順にいく
-  point_reverse_orders.each do |point|
+  point_reverse_order.each do |point|
     point_indexes[point].each do |prob_index|
       # AC済みなら追加点にはならないのでスキップ
-      next if results[player_id][prob_index]
+      next if results[player_index][prob_index]
 
       ans += 1
-      diff -= points[prob_index]
+      diff -= point
 
       # ACを取って負数にならなければ次の問題を見る
       next unless diff.negative?
