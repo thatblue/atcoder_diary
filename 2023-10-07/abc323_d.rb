d9863fa9b4c286b3797aaf76795f001eb7bb8bbd
thatblue <ancientblue@gmail.com>
2023-10-11 01:05:49 +0900

    Upsolve済みのコード

--- 2023-10-07/abc323_d.rb
+++ 2023-10-07/abc323_d.rb
@@ -1,33 +1,28 @@
-require 'ac-library-rb/priority_queue'
+# cf. https://atcoder.jp/contests/abc323/submissions/46311372
 
 n = gets.chomp.to_i
 
 slimes = Hash.new(0)
-size_queue = AcLibraryRb::PriorityQueue.new { |a, b| a < b }
-size_max = 0
 ans = 0
 n.times do
   size, count = gets.chomp.split.map(&:to_i)
-  size_queue.push(size)
   slimes[size] = count
-  size_max = [size_max, size].max
   ans += count
 end
 
-loop do
-  current_size = size_queue.pop
-  break if current_size.nil? || current_size > size_max
-  next if slimes[current_size] < 2
+slimes.keys.sort.each do |start_size|
+  current_size = start_size
 
-  size_2x = current_size * 2
-  count_2x = slimes[current_size] / 2
+  while slimes[current_size] > 1 do
+    size_2x = current_size * 2
+    count_2x = slimes[current_size] / 2
 
-  size_queue.push(size_2x) unless slimes.include?(size_2x)
+    slimes[current_size] = slimes[current_size] % 2
+    slimes[size_2x] += count_2x
+    ans -= count_2x
 
-  slimes[size_2x] += count_2x
-  slimes[current_size] = 1
-  ans -= count_2x
-  size_max = [size_max, size_2x].max
+    current_size = size_2x
+  end
 end
 
 puts ans
