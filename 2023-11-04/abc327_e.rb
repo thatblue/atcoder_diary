c92e832f0b6a0e0f69b4790a50b27810f8d875b8
thatblue <ancientblue@gmail.com>
2023-11-04 22:52:11 +0900

    E問題のオーバーランコード(未AC)

--- 2023-11-04/abc327_e.rb
+++ 2023-11-04/abc327_e.rb
@@ -3,45 +3,38 @@ include Math
 n = gets.chomp.to_i
 performances = gets.chomp.split.map(&:to_i)
 
-max_index = nil
-max_perf = -1
+def max_perf_and_index(performances)
+  max_index = nil
+  max_perf = -1
 
-performances.each_with_index do |perf, index|
-  if max_perf < perf
-    max_index = index
-    max_perf = perf
-  end
-end
-
-max_rate = -Float::INFINITY
-max_contests = 0
-(0..max_index).each do |start_index|
-  current_perf = 0
-  picked = []
-  performances[start_index..max_index].each do |perf|
-    if current_perf< perf
-      current_perf = perf
-      picked << perf
+  performances.each_with_index do |perf, index|
+    if max_perf < perf
+      max_index = index
+      max_perf = perf
     end
   end
 
-  next if max_contests > picked.count
-
-  max_contests = [picked.count, max_contests].max
-
-  k = picked.count
-  warareru = 0.to_f
-  waru = 0.to_f
-  picked.each_with_index do |perf, index|
-    i = index + 1
-    warareru += 0.9 ** (k - i) * perf
-    waru += 0.9 ** (k - i)
-  end
-
-  max_rate = [
-    warareru / waru - 1200 / Math.sqrt(k),
-    max_rate
-  ].max
+  [max_index, max_perf]
 end
 
-puts max_rate
+max_index = n
+picked_perfs = []
+3.times do
+  max_index, max_perf = max_perf_and_index(performances[0..max_index - 1])
+  picked_perfs.unshift max_perf
+
+  if max_index == 0
+    break
+  end
+end
+
+k = picked_perfs.count
+warareru = 0.to_f
+waru = 0.to_f
+picked_perfs.each_with_index do |perf, index|
+  i = index + 1
+  warareru += 0.9 ** (k - i) * perf
+  waru += 0.9 ** (k - i)
+end
+
+puts warareru / waru - 1200 / Math.sqrt(k)
