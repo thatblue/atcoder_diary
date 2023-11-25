eaf61612d33a40397464906c8c50a28ff23dc65c
thatblue <ancientblue@gmail.com>
2023-11-26 01:02:37 +0900

    C問題upsolve

--- 2023-11-25/abc330_c.rb
+++ 2023-11-25/abc330_c.rb
@@ -1,13 +1,12 @@
-include Math
-
 d = gets.chomp.to_i
 
 search_max = Math.sqrt(d).to_i
 ans = d
 1.upto(search_max) do |x|
-  y = Math.sqrt((d - x**2).abs).to_i
-  current_ans = ((x**2 + y**2) - d).abs
-  ans = [ans, current_ans].min
+  [Math.sqrt((d - x**2).abs).floor, Math.sqrt((d - x**2).abs).ceil].each do |y|
+    current_ans = ((x**2 + y**2) - d).abs
+    ans = [ans, current_ans].min
+  end
 end
 
 puts ans
