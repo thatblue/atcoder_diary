87cfcfe7f2d4b0ee4aaf4a1f06787f7e16414d7e
thatblue <ancientblue@gmail.com>
2023-11-26 01:10:56 +0900

    D問題のより平易な解法

--- 2023-11-25/abc330_d.rb
+++ 2023-11-25/abc330_d.rb
@@ -1,24 +1,15 @@
 n = gets.chomp.to_i
 
-map = []
-n.times do
-  map << gets.chomp.chars.map { |v| v == 'o' }
-end
-
 row_count = []
+col_count = Array.new(n, 0)
+map = []
 n.times do |i|
-  cumu_row = 0
-  row_count << map[i].map { |v| v ? cumu_row += 1 : cumu_row }
-end
-
-col_count = []
-n.times do |j|
-  cumu_col = 0
-  col = []
-  n.times do |i|
-    col << (map[i][j] ? cumu_col += 1 : cumu_col)
+  row = gets.chomp.chars.map { |v| v == 'o' }
+  map << row
+  row_count << row.count { |v| v }
+  row.each_with_index do |v, j|
+    col_count[j] += 1 if v
   end
-  col_count << col
 end
 
 ans = 0
@@ -26,15 +17,7 @@ n.times do |i|
   n.times do |j|
     next unless map[i][j]
 
-    # 左上
-    ans += (row_count[i][n-1] - row_count[i][j]) * (col_count[j][n-1] - col_count[j][i]) if i < n - 1 && j < n - 1
-    # 右上
-    ans += (row_count[i][j - 1]) * (col_count[j][n-1] - col_count[j][i]) if i < n - 1 && j > 0
-
-    # 左下
-    ans += (row_count[i][n-1] - row_count[i][j]) * (col_count[j][i - 1]) if i > 0 && j < n - 1
-    # 右下
-    ans += (row_count[i][j - 1]) * (col_count[j][i - 1]) if i > 0 && j > 0
+    ans += (row_count[i] - 1) * (col_count[j] - 1)
   end
 end
 
