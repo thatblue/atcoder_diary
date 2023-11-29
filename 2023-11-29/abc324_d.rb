7fb43e84b1df86a0edc53fc360077d42b72f1ad8
thatblue <ancientblue@gmail.com>
2023-11-29 23:52:11 +0900

    解説を読んでリファクタリングしたけどまだ1WAするコード

--- 2023-11-29/abc324_d.rb
+++ 2023-11-29/abc324_d.rb
@@ -3,40 +3,15 @@ include Math
 n = gets.chomp.to_i
 s = gets.chomp.chars.map(&:to_i)
 
-min = s.sort.join.to_i
-max = s.sort.reverse!.join.to_i
-
-min_sqrt = Math.sqrt(min).floor
+s_sorted = s.sort.join
+max = 10**n
 max_sqrt = Math.sqrt(max).ceil
 
-s_tally = s.tally
-s_tally.default = 0
-
 ans = 0
-num = min_sqrt
+1.upto(max_sqrt) do |num|
+  square_sorted = (num**2).to_s.chars.sort.join.rjust(n, '0')
 
-while num <= max_sqrt
-  square_chars = (num**2).to_s
-  square_chars_size = square_chars.size
-  tally = s_tally.dup
-  is_square_num = true
-
-  square_chars_size.times do |char_index|
-    char = square_chars[char_index].to_i
-    if tally[char].zero?
-      is_square_num = false
-      break
-    end
-
-    tally[char] -= 1
-  end
-
-  pp [num, num**2] if is_square_num
-  pp square_chars if is_square_num
-
-  ans += 1 if is_square_num
-
-  num += 1
+  ans += 1 if s_sorted == square_sorted
 end
 
 puts ans
