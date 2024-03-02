adfa6f46bee645f8fbe97ae921a6913b5a5c7973
thatblue <ancientblue@gmail.com>
2024-03-03 00:07:14 +0900

    E問題のupsolve

--- 2024-03-02/abc343_e.rb
+++ 2024-03-02/abc343_e.rb
@@ -1,124 +1,55 @@
-SUM_OF_CUBES = 3 * 7**3
-@volumes = gets.chomp.split.map(&:to_i)
-@volumes.unshift 0
+# 概ね解説コードの写経
+# cf. https://atcoder.jp/contests/abc343/editorial/9435
 
-# 面積の合計が一致しなければ絶対に成立しない
-if @volumes[1] + @volumes[2] * 2 + @volumes[3] * 3 != SUM_OF_CUBES
+SUM_OF_CUBES = 3 * 7**3
+v1, v2, v3 = gets.chomp.split.map(&:to_i)
+
+# 面積の合計がSUM_OF_CUBESに一致しなければ絶対に成立しない
+if v1 + v2 * 2 + v3 * 3 != SUM_OF_CUBES
   puts 'No'
   exit
 end
 
-def find(b, c)
-  imos = Array.new(15) { Array.new(15) { Array.new(15, 0) } }
-  # 位置関係が決まれば全体的にどこにあるかは本質的な問題ではないので、aの座標は固定して扱う
-  imos[0][0][0] = 1
-  imos[7][7][7] = -1
-  imos[b[:x]][b[:y]][b[:z]] += 1
-  imos[b[:x] + 7][b[:y] + 7][b[:z] + 7] -= 1
-  imos[c[:x]][c[:y]][c[:z]] += 1
-  imos[c[:x] + 7][c[:y] + 7][c[:z] + 7] -= 1
+def volume_double(a, b)
+  x_length = [0, [a[:x], b[:x]].min + 7 - [a[:x], b[:x]].max].max
+  y_length = [0, [a[:y], b[:y]].min + 7 - [a[:y], b[:y]].max].max
+  z_length = [0, [a[:z], b[:z]].min + 7 - [a[:z], b[:z]].max].max
 
-  # x方向への累積和
-  z = 0
-  while z <= 14
-    y = 0
-    while y <= 14
-      x = 1
-      while x <= 14
-        imos[x][y][z] += imos[x-1][y][z]
-        x += 1
-      end
-      y += 1
-    end
-    z += 1
-  end
-
-  # y方向への累積和
-  z = 0
-  while z <= 14
-    y = 1
-    while y <= 14
-      x = 0
-      while x <= 14
-        imos[x][y][z] += imos[x][y-1][z]
-        x += 1
-      end
-      y += 1
-    end
-    z += 1
-  end
-
-
-  # z方向への累積和
-  z = 1
-  while z <= 14
-    y = 0
-    while y <= 14
-      x = 0
-      while x <= 14
-        imos[x][y][z] += imos[x][y][z-1]
-        x += 1
-      end
-      y += 1
-    end
-    z += 1
-  end
-
-  counts = { 0 => 0, 1 => 0, 2 => 0, 3 => 0 }
-
-  z = 0
-  while z <= 13
-    y = 0
-    while y <= 13
-      x = 0
-      while x <= 13
-        counts[imos[x][y][z]] += 1
-        return false if imos[x][y][z].positive? && counts[imos[x][y][z]] > @volumes[imos[x][y][z]]
-      end
-      y += 1
-    end
-    z += 1
-  end
-
-  true
+  x_length * y_length * z_length
 end
 
-visited = Hash.new { |hash, key| hash[key] = {} }
+def volume_triple(a, b, c)
+  x_length = [0, [a[:x], b[:x], c[:x]].min + 7 - [a[:x], b[:x], c[:x]].max].max
+  y_length = [0, [a[:y], b[:y], c[:y]].min + 7 - [a[:y], b[:y], c[:y]].max].max
+  z_length = [0, [a[:z], b[:z], c[:z]].min + 7 - [a[:z], b[:z], c[:z]].max].max
 
-b_x = 0
-while b_x <= 7
-  b_y = 0
-  while b_y <= 7
-    b_z = 0
-    while b_z <= 7
-      c_x = 0
-      while c_x <= 7
-        c_y = 0
-        while c_y <= 7
-          c_z = 0
-          while c_z <= 7
-            b_id = b_x**7 + b_y * 7 + b_z
-            c_id = c_x**7 + c_y * 7 + c_z
-            next if visited[c_id][b_id]
+  x_length * y_length * z_length
+end
 
-            if find({ x: b_x, y: b_y, z: b_z }, { x: c_x, y: c_y, z: c_z })
-              puts 'Yes'
-              puts ([0, 0, 0] + [b_x, b_y, b_z] + [c_x, c_y, c_z]).join ' '
-              exit
-            end
-            visited[b_id][c_id] = true
-            pp visited
-            c_z += 1
+# aの位置は固定できるので、bとcの座標のみ動かしていく
+8.times do |b_x|
+  8.times do |b_y|
+    8.times do |b_z|
+      (-6..7).each do |c_x|
+        (-6..7).each do |c_y|
+          (-6..7).each do |c_z|
+            current_v3 = volume_triple({ x: 0, y: 0, z: 0 }, { x: b_x, y: b_y, z: b_z }, { x: c_x, y: c_y, z: c_z })
+
+            double_ab = volume_double({ x: 0, y: 0, z: 0 }, { x: b_x, y: b_y, z: b_z })
+            double_bc = volume_double({ x: b_x, y: b_y, z: b_z }, { x: c_x, y: c_y, z: c_z })
+            double_ac = volume_double({ x: 0, y: 0, z: 0 }, { x: c_x, y: c_y, z: c_z })
+            current_v2 = double_ab + double_bc + double_ac - current_v3 * 3
+
+            next if current_v2 != v2 || current_v3 != v3
+
+            puts 'Yes'
+            puts [0, 0, 0, b_x, b_y, b_z, c_x, c_y, c_z].join ' '
+            exit
           end
-          c_y += 1
         end
-        c_x += 1
       end
-      b_z += 1
     end
-    b_y += 1
   end
-  b_x += 1
 end
 
 puts 'No'
